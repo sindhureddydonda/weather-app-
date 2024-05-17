73bab0aba3897782e1d35bda6d6aa0ba55ffcd5e
sindhureddydonda <sindhureddydonda2000@gmail.com>
2024-05-17 19:29:46 -0400

    location

--- src/components/LocationField.jsx
+++ src/components/LocationField.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+const cityNameField = { 
+  borderRadius: "15px",
+  width: "334px",
+  height: "42px",
+  borderColour: "#4C4DDB",
 
+
+
+
+}
 function LocationField({ location, setLocation, searchLocation }) {
   return (
     <div className="search">
       <input
+      style={ cityNameField}
         value={location}
         onChange={(event) => setLocation(event.target.value)}
         onKeyPress={searchLocation}
